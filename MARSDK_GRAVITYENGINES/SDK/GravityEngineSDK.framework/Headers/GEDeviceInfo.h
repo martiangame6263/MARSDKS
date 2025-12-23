e71d5be5c3d7ec75a94d659de0843b63acc982d6
xiaoyu <810135086@qq.com>
2025-12-23 17:31:03 +0800

    引力引擎插件

---
+++ MARSDK_GRAVITYENGINES/SDK/GravityEngineSDK.framework/Headers/GEDeviceInfo.h
@@ -0,0 +1,40 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXTERN NSString *const VERSION;
+
+@interface GEDeviceInfo : NSObject
+
++ (GEDeviceInfo *)sharedManager;
+
+
+@property (nonatomic, copy) NSString *uniqueId;
+@property (nonatomic, copy) NSString *deviceId;
+@property (nonatomic, copy) NSString *appVersion;
+@property (nonatomic, assign) int buildVersionCode;
+@property (nonatomic, readonly) BOOL isFirstOpen;
+@property (nonatomic, copy) NSString *libName;
+@property (nonatomic, copy) NSString *libVersion;
+
++ (NSString *)libVersion;
++ (NSString*)bundleId;
+
+- (void)ge_updateData;
+- (NSDictionary *)ge_collectProperties;
+
++ (NSDate *)ge_getInstallTime;
+
+- (NSDictionary *)getAutomaticData;
+
++ (NSString *)currentRadio;
+
++ (NSTimeInterval)uptime;
++ (NSString *)bootTimeSec;
++ (NSDate *)systemUpdateTime;
+
+- (NSString*)ge_iphoneType;
+- (NSString*)getIdentifier;
+@end
+
+NS_ASSUME_NONNULL_END
