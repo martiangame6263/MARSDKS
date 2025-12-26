886324ea9076855fb42b6bf121876faf47853a16
xiaoyu <810135086@qq.com>
2025-12-26 15:54:45 +0800

    新增广告渠道

---
+++ MARSDKADVERT/SDK/MARSDK_ADVERT.framework/Headers/ADVERT.h
@@ -0,0 +1,21 @@
+//
+//  ADVERT.h
+//  ADVERT
+//
+//  Created by 火星人 on 2022/3/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ADVERT.
+FOUNDATION_EXPORT double ADVERTVersionNumber;
+
+//! Project version string for ADVERT.
+FOUNDATION_EXPORT const unsigned char ADVERTVersionString[];
+//
+//#import <MARSDK_ADVERT/ADVERT.h>
+//#import <MARSDK_ADVERT/EPlatform.h>
+//#import <MARSDK_ADVERT/MARSDK_ADVERT.h>
+
+// In this header, you should import all the public headers of your framework
+// using statements like #import <ADVERT/PublicHeader.h>
